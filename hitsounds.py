36760798e88fcc6fb55981d39367df46ec56451b
Like4Schnitzel <78065512+Like4Schnitzel@users.noreply.github.com>
2021-06-24 00:18:48 +0200

    Update hitsounds.py

--- hitsounds.py
+++ hitsounds.py
@@ -16,15 +16,20 @@ for filename in os.listdir():
         mp3names.append(file)
 
 chance = 0
+#infinite loop
 while True:
+    #setting 0.2 second cooldown so files only play once on hit
     time.sleep(0.2)
+    #generating random number for playing a sound if there is more than 1 sound file
     if(len(mp3names) > 1):
         chancenew = chance
         while chancenew == chance:
             chancenew = random.randint(0, len(mp3names) - 1)
         chance = chancenew
+    #checking if a key is held down
     try:
         if keyboard.read_key():
             sound(chance, mp3names)
     except:
+        #does nothing
         print("", end="")
